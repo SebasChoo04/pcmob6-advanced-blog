424d82bf7c16e0874b96e67b62bc99eee71556af
sebastianchoo <sebastian_choo>
2021-07-02 02:00:13 +0800

    IndexScreen completed

--- screens/BlogStack.js
+++ screens/BlogStack.js
@@ -8,7 +8,21 @@ const InnerStack = createStackNavigator();
 export default function BlogStack() {
   return (
     <InnerStack.Navigator>
-      <InnerStack.Screen name="Index" component={IndexScreen} />
+      <InnerStack.Screen name="Index" component={IndexScreen} options={{
+        title: "Blog",
+        headerStyle: {
+          backgroundColor: "yellow",
+          height: 100,
+          shadowColor: "black",
+          shadowOpacity: 0.2,
+          shadowRadius: 5,
+        },
+        headerTintColor: "#f55",
+        headerTitleStyle: {
+          fontSize: 24,
+          fontWeight: "bold",
+        }
+      }}/>
       <InnerStack.Screen name="Add" component={CreateScreen} />
       <InnerStack.Screen name="Edit" component={EditScreen}/>
     </InnerStack.Navigator>
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,13 +1,99 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { commonStyles } from "../styles/commonStyles";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { API, API_POSTS } from "../constants/API";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function IndexScreen({ navigation }) {
+export default function IndexScreen({ navigation, route }) {
+  const [posts, setPosts] = useState([]);
+
+  // This is to set up the top right button
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Ionicons
+            name="ios-create-outline"
+            size={30}
+            color="black"
+            style={{
+              color: "#f55",
+              marginRight: 10,
+            }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  async function getPosts() {
+    const token = await AsyncStorage.getItem("token");
+    try {
+      const response = await axios.get(API + API_POSTS, {
+        headers: { Authorization: `JWT ${token}` },
+      })
+      console.log(response.data);
+      setPosts(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  function addNote() {
+    navigation.navigate("Add Screen");
+  }
+
+  // This deletes an individual note
+  function deleteNote(id) {
+    console.log("Deleting " + id);
+    // To delete that item, we filter out the item we don't want
+    setNotes(notes.filter((item) => item.id !== id));
+  }
+
+  // The function to render each row in our FlatList
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>{item.title}</Text>
+        <TouchableOpacity onPress={() => deleteNote(item.id)}>
+          <Ionicons name="trash" size={16} color="#944" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View style={commonStyles.container}>
-      <Text>Index Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={posts}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
