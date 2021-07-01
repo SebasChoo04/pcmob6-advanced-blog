df0c9c848e542dde0a0cfca93213739c0298348e
sebastianchoo <sebastian_choo>
2021-07-02 02:13:21 +0800

    Tab bar icons added

--- screens/LoggedInTabStack.js
+++ screens/LoggedInTabStack.js
@@ -3,12 +3,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BlogStack from './BlogStack';
 import AccountScreen from './AccountScreen';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
 export default function LoggedInStack(){
   return (
-      <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Blog') {
+            iconName = "comments"
+          } else if (route.name === 'Settings') {
+            iconName = "cog"
+          }
+          // You can return any component that you like here!
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
         <Tab.Screen name="Blog" component={BlogStack} />
         <Tab.Screen name="Settings" component={AccountScreen} />
       </Tab.Navigator>
